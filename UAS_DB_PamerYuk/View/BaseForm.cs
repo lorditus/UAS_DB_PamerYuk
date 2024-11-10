0b7d6987f92d4106302a16e08ff619595752e29f
StaticKev <statickev321@gmail.com>
2024-11-10 21:16:16 +0700

    Initial Commit!

---
+++ UAS_DB_PamerYuk/View/BaseForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UAS_DB_PamerYuk
+{
+    public partial class BaseForm : Form
+    {
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BaseForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
